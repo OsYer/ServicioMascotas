a4c95bf0e58cdb20fbf3cab660e121156a034461
OsYer <20210720@uthh.edu.mx>
2025-03-14 16:12:56 -0600

    Agregar archivos de proyecto.

---
+++ ServicioMascotas/IMascotas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServicioMascotas
+{
+	// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IMascotas" en el código y en el archivo de configuración a la vez.
+	[ServiceContract]
+	public interface IMascotas
+	{
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+		string ProbarConexion();
+	}
+}
---
+++ ServicioMascotas/Mascotas.svc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Npgsql;
+
+namespace ServicioMascotas
+{
+	// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Mascotas" en el código, en svc y en el archivo de configuración a la vez.
+	// NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Mascotas.svc o Mascotas.svc.cs en el Explorador de soluciones e inicie la depuración.
+	public class Mascotas : IMascotas
+	{
+        private string _connectionString = ConfigurationManager.ConnectionStrings["PostgreSQLConn"].ConnectionString;
+
+        public string ProbarConexion()
+        {
+            try
+            {
+                using (var conn = new NpgsqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    return "¡Conexión exitosa a PostgreSQL!";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error de conexión: " + ex.Message;
+            }
+        }
+    }
+}
